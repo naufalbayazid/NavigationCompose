cc0c35703916e070cacf887c260d5fed593373ce
naufalbayazid <naufalichwanbayazid@gmail.com>
2024-11-21 20:33:47 +0700

    menambhakan pilihan yang telah disediakan pada object matakuliah

---
+++ MataKuliah.kt
@@ -0,0 +1,14 @@
+package com.example.navigationcompose.data
+
+object MataKuliah {
+    val options = listOf(
+        "Pemrograman Aplikasi Mobile",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vision"
+
+
+    )
+}
